74ea9d51a12b2b7bbd677017611e7cb1d4382adf
Julio J <jjyepez@gmail.com>
2018-05-21 16:58:37 -0400

    Ajustes menores

--- script.js
+++ script.js
@@ -127,6 +127,7 @@ var emojis;
                 $contCat.appendChild( $ico );
                 $ico.addEventListener('click', e => {
                     const $input = document.getElementById('chat-input');
+                    $input.style.paddingRight = '2rem';
                     $input.value += e.target.innerHTML;
                     // document.querySelector('.jjPopOver').classList.add('oculto'); --- opcional
                     $input.focus();
